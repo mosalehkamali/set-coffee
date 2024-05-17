a47bed53912dcba1fe640c03edda245341011ab1
mosalehkamali <mosalehkamali@gmail.com>
2024-05-17 22:41:10 +0330

    create articles section

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
 import Navbar from "@/components/modules/navbar/Navbar";
+import Articles from "@/components/templates/index/articles/Articles";
 import Banner from "@/components/templates/index/banner/banner";
 import Latest from "@/components/templates/index/latest/latest";
+import Promote from "@/components/templates/index/promote/Promote";
 export default function Home() {
   return (
     <>
       <Navbar />
       <Banner />
       <Latest />
+      <Promote />
+      <Articles />
     </>
   );
 }
