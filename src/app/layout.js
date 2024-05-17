9be15f4a3e36348b738d2d71c6983a8a685be587
mosalehkamali <mosalehkamali@gmail.com>
2024-05-17 21:36:44 +0330

    create latest template

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,23 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-
+import AOSInit from "@/utils/aos";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "شرکت فنجان داغ خوارزمی - SET Coffee | فروشگاه اینترنتی قهوه ست",
   description: "Mohammad Saleh Kamali project with Next.js v13",
-  icons:{
-    icon:"/images/favicon.png"
-  }
+  icons: {
+    icon: "/images/favicon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fa">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AOSInit />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Navbar from "@/components/modules/navbar/Navbar";
 import Banner from "@/components/templates/index/banner/banner";
+import Latest from "@/components/templates/index/latest/latest";
 export default function Home() {
   return (
     <>
       <Navbar />
       <Banner />
+      <Latest />
     </>
   );
 }
