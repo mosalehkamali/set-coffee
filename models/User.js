cdc8512315450ebfeabf3c0ce6049ad47505712b
mosalehkamali <mosalehkamali@gmail.com>
2024-05-18 17:37:03 +0330

    create user model

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const { default: mongoose } = require("mongoose");
+
+const schema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: false,
+  },
+  password: {
+    type: String,
+    required: false,
+  },
+  role: {
+    type: String,
+    requiered: true,
+    default: "USER",
+  },
+  refreshToken: String,
+});
+
+const model = mongoose.models.User || mongoose.model("User", schema);
+
+export default model;
