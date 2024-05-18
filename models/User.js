b9961d18b6652874b9e028b914ba5898c8048715
mosalehkamali <mosalehkamali@gmail.com>
2024-05-18 19:14:59 +0330

    handle register in signin api

--- models/User.js
+++ models/User.js
@@ -1,29 +1,34 @@
 const { default: mongoose } = require("mongoose");
 
-const schema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const schema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: false,
+    },
+    password: {
+      type: String,
+      required: false,
+    },
+    role: {
+      type: String,
+      requiered: true,
+      default: "USER",
+    },
+    refreshToken: String,
   },
-  phone: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: false,
-  },
-  password: {
-    type: String,
-    required: false,
-  },
-  role: {
-    type: String,
-    requiered: true,
-    default: "USER",
-  },
-  refreshToken: String,
-});
+  {
+    timestamps: true,
+  }
+);
 
 const model = mongoose.models.User || mongoose.model("User", schema);
 
--- src/app/api/auth/signup/route.js
+++ src/app/api/auth/signup/route.js
@@ -1,6 +1,34 @@
+import { hasher, tokenGenrator } from "@/utils/auth";
 import connectToDB from "base/configs/db";
+import userModel from "base/models/User";
 
-export async function GET(req) {
+export async function POST(req) {
   await connectToDB();
-  return Response.json({ message: "signup api route" });
+  try {
+    const reqBody = await req.json();
+    const { name, phone, email, password } = reqBody;
+
+    const hashedPassword = password ? await hasher(password) : password;
+    const users = await userModel.find({});
+    const user = await userModel.create({
+      name,
+      phone,
+      email,
+      password: hashedPassword,
+      role: users.length > 0 ? "USEER" : "ADMIN",
+    });
+
+    const token = tokenGenrator({ name, phone });
+
+    return Response.json(
+      { message: "User Registered Successfully :))" },
+      {
+        status: 201,
+        headers: { "Set-Cookie": `token=${token};path=/;httpOnly=true` },
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    return Response.json({ error: "UnKnown Internal Server Error !!!" });
+  }
 }
