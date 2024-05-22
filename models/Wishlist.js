d6108478ad612857de0e4434d30651449c77d213
mosalehkamali <mosalehkamali@gmail.com>
2024-05-22 22:13:29 +0330

    create wishlist api

--- models/Wishlist.js
+++ models/Wishlist.js
@@ -2,16 +2,21 @@ const mongoose = require("mongoose");
 require("./Product");
 require("./User");
 
-const schema = new mongoose.Schema({
-  user: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
+const schema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+    product: {
+      type: mongoose.Types.ObjectId,
+      ref: "Product",
+    },
   },
-  product: {
-    type: mongoose.Types.ObjectId,
-    ref: "Product",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const model = mongoose.models.Wishlist || mongoose.model("Wishlist", schema);
 
---
+++ src/app/api/wishlist/route.js
@@ -0,0 +1,45 @@
+import connectToDB from "base/configs/db";
+import userModel from "base/models/User";
+import productModel from "base/models/Product";
+import wishlistModel from "base/models/Wishlist";
+
+export async function POST(req) {
+  try {
+    await connectToDB();
+    const { user, product } = await req.json();
+    const userData = await userModel.findOne({ _id: user });
+    const productData = await productModel.findOne({ _id: product });
+    if (!userData || !productData) {
+      return Response.json(
+        { message: "Invalid Data !!!" },
+        {
+          status: 409,
+        }
+      );
+    }
+    const wishlist = await wishlistModel.create({ user, product });
+
+    if (!wishlist) {
+      return Response.json(
+        { message: "Failed to create wishlist !!!" },
+        {
+          status: 500,
+        }
+      );
+    }
+    return Response.json(
+      { message: "Wishlist created successfully" },
+      {
+        status: 201,
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    return Response.json(
+      { message: "UnKnown Internal Server Error!!!" },
+      {
+        status: 500,
+      }
+    );
+  }
+}
