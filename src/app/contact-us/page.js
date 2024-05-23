669b382b622d68ede89c32b24c890a660da61a97
mosalehkamali <mosalehkamali@gmail.com>
2024-05-23 18:48:35 +0330

    show shop location in leaflet map

--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -3,8 +3,10 @@ import Footer from "@/components/modules/footer/Footer";
 import Navbar from "@/components/modules/navbar/Navbar";
 import Form from "@/components/templates/contact-us/Form";
 import Information from "@/components/templates/contact-us/Information";
+import Map from "@/components/templates/contact-us/Map";
 import styles from "@/styles/contact-us.module.css";
 import { authUser } from "@/utils/useHeaders";
+import Link from "next/link";
 
 const page = async () => {
   const user = await authUser();
@@ -13,6 +15,43 @@ const page = async () => {
     <>
       <Navbar isLogin={user ? true : false} />
       <Breadcrumb route={"تماس با ما"} />
+
+      <div className={styles.container}>
+        <main className={styles.maps}>
+          <section>
+            <Map
+              position={[35.72021225108499, 51.42222691580869]}
+              center={[35.72021225108499, 51.42222691580869]}
+            >
+              <span> فروشگاه ما</span>
+              <h3>آدرس فروشگاه حضوری قهوه ست (شعبه جم)</h3>
+              <p>
+                تهران – خ کریمخان زند – خ قائم مقام فراهانی – ابتدای خ فجر(جم) –
+                شماره ۱۰
+              </p>
+              <p>021-88305827</p>
+              <Link href="/about-us">درباره فروشگاه</Link>
+            </Map>
+          </section>
+          <section>
+            <Map
+              position={[28.673080, 57.738913]}
+              center={[28.673080, 57.738913]}
+            >
+              <span> فروشگاه ما</span>
+              <h3>آدرس فروشگاه حضوری قهوه ست (شعبه جم)</h3>
+              <p>
+                تهران – خ کریمخان زند – خ قائم مقام فراهانی – ابتدای خ فجر(جم) –
+                شماره ۱۰
+              </p>
+              <p>021-88305827</p>
+              <Link href="/about-us">درباره فروشگاه</Link>
+            </Map>
+          </section>
+        </main>
+      </div>
+
+
       <div className={styles.container}>
         <div className={styles.contents}>
           <Form />
