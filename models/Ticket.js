984df07f96e94421d0a008e6afadd3a32bc38d62
mosalehkamali <mosalehkamali@gmail.com>
2024-05-25 09:37:39 +0330

    create department  model

--- models/Ticket.js
+++ models/Ticket.js
@@ -1,31 +1,37 @@
 const mongoose = require("mongoose");
 require("./User");
+require("./Department");
 
-const schema = new mongoose.Schema({
-  titile: {
-    type: String,
-    required: true,
+const schema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    department: {
+      type: mongoose.Types.ObjectId,
+      ref: "Department",
+      required: true,
+    },
+    priority: {
+      type: Number,
+      default: 3,
+      enum: [1, 2, 3],
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-  body: {
-    type: String,
-    required: true,
-  },
-  department: {
-    type: mongoose.Types.ObjectId,
-    ref: "Department",
-    required: true,
-  },
-  priority: {
-    type: Number,
-    default: 3,
-    enum: [1, 2, 3],
-  },
-  user: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const model = mongoose.models.Ticket || mongoose.model("Ticket", schema);
 
