51cd631b1e35fb1d7b4fa1d39d180364796f1b0f
mosalehkamali <mosalehkamali@gmail.com>
2024-05-27 01:07:41 +0330

    admin panel route protection

--- src/components/layouts/AdminPanelLayout.js
+++ src/components/layouts/AdminPanelLayout.js
@@ -2,8 +2,18 @@ import React from "react";
 import styles from "./adminPanelLayout.module.css";
 import Sidebar from "@/components/modules/p-admin/Sidebar";
 import Topbar from "@/components/modules/p-admin/Topbor";
+import { authUser } from "@/utils/serverHelpers";
+import { redirect } from "next/navigation";
 
-const Layout = ({ children }) => {
+const AdminPanelLayout = async ({ children }) => {
+  const user  = await authUser()
+  if(!user){
+    return redirect("/login-register")
+  }
+  if(user.role !== "ADMIN"){
+    return redirect("/p-user")
+
+  }
   return (
     <div className={styles.layout}>
       <section className={styles.section}>
@@ -17,4 +27,4 @@ const Layout = ({ children }) => {
   );
 };
 
-export default Layout;
+export default AdminPanelLayout;
