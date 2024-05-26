f6254df823a81abcdabb4eb5b0aae134678ccf11
mosalehkamali <mosalehkamali@gmail.com>
2024-05-26 22:59:30 +0330

    create admin panel

---
+++ src/app/p-admin/page.js
@@ -0,0 +1,40 @@
+import React from "react";
+import AdminPanelLayout from "@/components/layouts/AdminPanelLayout";
+
+import styles from "@/styles/p-admin/index.module.css";
+import Box from "@/components/modules/infoBox/InfoBox";
+
+import TicketModel from "base/models/Ticket";
+import UserModel from "base/models/User";
+import ProductModel from "base/models/Product";
+import connectToDB from "base/configs/db";
+
+async function AdminHomePage() {
+  connectToDB();
+  const tickets = await TicketModel.find({}).lean();
+  const users = await UserModel.find({}).lean();
+  const products = await ProductModel.find({}).lean();
+
+  return (
+    <AdminPanelLayout>
+      <main>
+        <section className={styles.dashboard_contents}>
+          <Box title="مجموع تیکت های دریافتی" value={tickets.length} />
+          <Box title="مجموع محصولات سایت" value={products.length} />
+          <Box title="مجموع سفارشات" value="333" />
+          <Box title="مجموع کاربر های سایت" value={users.length} />
+        </section>{" "}
+        <div className={styles.dashboard_charts}>
+          <section>
+            <p>آمار فروش</p>
+          </section>
+          <section>
+            <p>نرخ رشد</p>
+          </section>
+        </div>
+      </main>
+    </AdminPanelLayout>
+  );
+}
+
+export default AdminHomePage;
