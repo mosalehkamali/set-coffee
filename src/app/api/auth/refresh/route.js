ca199aca153946cf9081fab9126f0ee1126140ab
mosalehkamali <mosalehkamali@gmail.com>
2024-05-31 22:35:04 +0330

    update refresh token api

--- src/app/api/auth/refresh/route.js
+++ src/app/api/auth/refresh/route.js
@@ -3,13 +3,24 @@ import userModel from "base/models/User";
 import { verify } from "jsonwebtoken";
 import { cookies } from "next/headers";
 
-export async function POST(req) {
+export async function GET(req) {
   try {
     const refreshToken = cookies().get("refresh-token").value;
 
+   
+
+    if (!refreshToken) {
+      return Response.json(
+        { message: "user is not login" },
+        {
+          status: 401,
+        }
+      );
+    }
+
     const user = await userModel.findOne({ refreshToken });
 
-    if (!refreshToken || !user) {
+    if (!user) {
       return Response.json(
         { message: "user is not login" },
         {
