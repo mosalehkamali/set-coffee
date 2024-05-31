6b37a17301211f4e9c73c35a67e3cbc9ab915e82
mosalehkamali <mosalehkamali@gmail.com>
2024-05-31 18:59:16 +0330

    create refresh token api

---
+++ src/app/api/auth/refresh/route.js
@@ -0,0 +1,36 @@
+import { tokenGenrator } from "@/utils/auth";
+import userModel from "base/models/User";
+import { verify } from "jsonwebtoken";
+import { cookies } from "next/headers";
+
+export async function POST(req) {
+  try {
+    const refreshToken = cookies().get("refresh-token").value;
+
+    const user = await userModel.findOne({ refreshToken });
+
+    if (!refreshToken || !user) {
+      return Response.json(
+        { message: "user is not login" },
+        {
+          status: 401,
+        }
+      );
+    }
+
+    verify(refreshToken, process.env.RefreshTokenPrivateKey);
+
+    const token = tokenGenrator({ phone: user.phone });
+
+    return Response.json(
+      { message: "Access token refreshed successfuly" },
+      {
+        status: 200,
+        headers: { "Set-Cookie": `token=${token};path=/;httpOnly=true` },
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    return Response.json({ error: "UnKnown Internal Server Error !!!" });
+  }
+}
